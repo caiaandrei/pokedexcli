70fcb27a2211f736790023b9fde8eb883a110651
Caia Andrei <caiaandrei14@hotmail.com>
2025-11-18 01:31:24 +0200

    add catch and inspect

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand/v2"
 	"net/http"
 	"os"
 	pokecache "pokedexcli/internal"
@@ -12,6 +13,16 @@ import (
 	"strings"
 	"time"
 )
+
+type pokedexItem struct {
+	name   string
+	height int
+	weight int
+	stats  string
+	types  string
+}
+
+var pokedex map[string]pokedexItem
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -21,6 +32,7 @@ func main() {
 		cache:    *pokecache.NewCache(5 * time.Second),
 		url:      "https://pokeapi.co/api/v2/location-area/",
 	}
+	pokedex = make(map[string]pokedexItem)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
@@ -80,6 +92,16 @@ func getCommands() map[string]cliCommand {
 			name:        "explore",
 			description: "Get a list of all the Pokémon from a location",
 			callback:    commandExpore,
+		},
+		"catch": {
+			name:        "catch",
+			description: "Catch a Pokemon and at it to your Pokedex",
+			callback:    commandCatch,
+		},
+		"inspect": {
+			name:        "inspect",
+			description: "Inspect a Pokemon from your Pokedex",
+			callback:    commandInspect,
 		},
 	}
 }
@@ -156,8 +178,86 @@ func commandMap(config *config) error {
 	return getLocations(config, "forward")
 }
 
+func commandCatch(config *config) error {
+	if len(config.args) == 0 {
+		return fmt.Errorf("provide a pokemon name for the catch command")
+	}
+
+	fmt.Printf("Throwing a Pokeball at %s...\n", config.args[0])
+
+	resp, err := http.Get("https://pokeapi.co/api/v2/pokemon/" + config.args[0])
+	if err != nil {
+		return nil
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	var pokemonResp pokemonResp
+
+	json.Unmarshal(body, &pokemonResp)
+
+	caught := rand.Float64() > float64(pokemonResp.BaseExp)/1000
+	if caught {
+		fmt.Printf("%s was caught!\n", config.args[0])
+		pokedex[config.args[0]] = pokedexItem{
+			name:   config.args[0],
+			height: pokemonResp.Height,
+			weight: pokemonResp.Weight,
+			stats:  formatStats(pokemonResp.Stats),
+			types:  formatTypes(pokemonResp.Types),
+		}
+	} else {
+		fmt.Printf("%s escaped!\n", config.args[0])
+	}
+
+	fmt.Println(pokedex)
+	return nil
+}
+
+func formatStats(stats []statsResp) string {
+	var result string
+
+	for i := range stats {
+		result += fmt.Sprintf("	-%s: %d \n", stats[i].Stat.Name, stats[i].BaseStat)
+	}
+
+	return result
+}
+
+func commandInspect(config *config) error {
+	if len(config.args) == 0 {
+		return fmt.Errorf("provide a pokemon name for the inspect command")
+	}
+
+	pokeName := config.args[0]
+
+	pokemon, exists := pokedex[pokeName]
+
+	if !exists {
+		fmt.Println("you have not caught that pokemon")
+		return nil
+	}
+
+	fmt.Printf("Name: %s\n Height: %d\n Weight: %d\n Stats:\n%s Types:\n%s", pokemon.name, pokemon.height, pokemon.weight, pokemon.stats, pokemon.types)
+
+	return nil
+}
+
+func formatTypes(types []typeResp) string {
+	var result string
+
+	for i := range types {
+		result += fmt.Sprintf("	- %s\n", types[i].Type.Name)
+	}
+
+	return result
+}
+
 func commandExpore(config *config) error {
-	fmt.Println(config.args)
 	if len(config.args) == 0 {
 		return fmt.Errorf("provide a location for the explore command")
 	}
@@ -179,8 +279,8 @@ func commandExpore(config *config) error {
 		return err
 	}
 
-	for i := range locationResp.PokemonResp {
-		fmt.Println(locationResp.PokemonResp[i].Pokemon.Name)
+	for i := range locationResp.PokemonEnc {
+		fmt.Println(locationResp.PokemonEnc[i].Pokemon.Name)
 	}
 
 	return nil
@@ -232,7 +332,6 @@ func getLocations(config *config, direction string) error {
 }
 
 type locationsResp struct {
-	// count   int
 	Next     string
 	Previous string
 	Results  []location
@@ -240,7 +339,6 @@ type locationsResp struct {
 
 type location struct {
 	Name string
-	//Url  string
 }
 
 type config struct {
@@ -252,13 +350,38 @@ type config struct {
 }
 
 type locationResp struct {
-	PokemonResp []pokemonResp `json:"pokemon_encounters"`
+	PokemonEnc []pokemonEnc `json:"pokemon_encounters"`
 }
 
-type pokemonResp struct {
+type pokemonEnc struct {
 	Pokemon pokemon `json:"pokemon"`
 }
 
 type pokemon struct {
+	Name string
+}
+
+type pokemonResp struct {
+	BaseExp int `json:"base_experience"`
+	Height  int
+	Weight  int
+	Stats   []statsResp
+	Types   []typeResp
+}
+
+type statsResp struct {
+	BaseStat int `json:"base_stat"`
+	Stat     stat
+}
+
+type stat struct {
+	Name string
+}
+
+type typeResp struct {
+	Type typePoke
+}
+
+type typePoke struct {
 	Name string
 }
